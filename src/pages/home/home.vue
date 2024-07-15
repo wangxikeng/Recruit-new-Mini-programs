a150655327c0ade8001327c2f1c316a308e0f386
wangxikeng <2835884569@qq.com>
2024-07-15 17:11:37 +0800

    commit:完成小程序的基础配置，包括组件库安装，页面路由基本配置，ts 报错与开发优化

---
+++ src/pages/home/home.vue
@@ -0,0 +1,7 @@
+<template>
+  <view>我是首页</view>
+</template>
+<script setup lang="ts">
+import { ref, reactive } from "vue";
+</script>
+<style lang="scss" scoped></style>
---
+++ src/pages/interview/interview.vue
@@ -0,0 +1,8 @@
+<template>
+    <view>我是报名页</view>
+  </template>
+  <script setup lang="ts">
+  import { ref, reactive } from "vue";
+  </script>
+  <style lang="scss" scoped></style>
+  

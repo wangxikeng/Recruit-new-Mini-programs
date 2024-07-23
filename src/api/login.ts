a02f596c337d969972c1d5bd178c4dcf3c65495d
wangxikeng <2835884569@qq.com>
2024-07-23 16:50:27 +0800

    fix: 添加了请求头

--- src/api/login.ts
+++ src/api/login.ts
@@ -1,6 +1,5 @@
 import type { IData } from '@/types/baseType'
 import type { IUser } from '@/types/user'
-import type { IUser } from '@/types/user'
 import { http } from '@/utils/http'
 
 export const logIn = () => {
@@ -8,9 +7,20 @@ export const logIn = () => {
     url: '/user/login/user',
     method: 'POST',
     data: {
-      account: '3223004304',
+      account: '3123004529',
       password: '123456'
     }
+  })
+}
+
+/**
+ * 获取到最新的一条通知
+ */
+
+export const getNewNotice = () => {
+  return http({
+    url: '/notice/getNotice',
+    method: 'GET'
   })
 }
 

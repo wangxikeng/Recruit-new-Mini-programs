56a5819b7f761e47141619733b50f77e0856ac2c
wangxikeng <2835884569@qq.com>
2024-07-23 11:48:09 +0800

    fix: 修复登录请求问题

--- src/utills/http.ts
+++ src/utills/http.ts
@@ -1,47 +1,47 @@
-import { useUserStore } from "@/stores/modules/user";
-import { __BASE_URL__, __API_TIMEOUT__ } from "@/constant";
-import { Data } from "../types/baseType";
+import { useUserStore } from '@/stores/modules/user'
+import { __BASE_URL__, __API_TIMEOUT__ } from '@/constant'
+import { IData } from '../types/baseType'
 
 //添加拦截器
 const httpInterceptor = {
   //拦截前触发
   invoke(option: UniApp.RequestOptions) {
-    if (!option.url.startsWith("http")) {
-      option.url = __BASE_URL__ + option.url;
+    if (!option.url.startsWith('http')) {
+      option.url = __BASE_URL__ + option.url
     }
-    option.timeout = __API_TIMEOUT__;
-    const memberStore = useUserStore();
+    option.timeout = __API_TIMEOUT__
+    const memberStore = useUserStore()
     // 取 token ，然后附在请求头上
-  },
-};
-uni.addInterceptor("request", httpInterceptor);
-uni.addInterceptor("upLoadFile", httpInterceptor);
+  }
+}
+uni.addInterceptor('request', httpInterceptor)
+uni.addInterceptor('upLoadFile', httpInterceptor)
 
 //封装网络请求
 export const http = <T>(Option: UniApp.RequestOptions) => {
-  return new Promise<Data<T>>((resolve, reject) => {
+  return new Promise<IData<T>>((resolve, reject) => {
     uni.request({
       ...Option,
       //请求成功
       success(res) {
-        if (res.statusCode >= 200 && res.statusCode < 200) {
-          resolve(res.data as Data<T>);
+        if (res.statusCode >= 200 && res.statusCode < 300) {
+          resolve(res.data as IData<T>)
         } else {
           uni.showToast({
-            icon: "none",
-            title: (res.data as Data<T>).msg || "请求错误",
-          });
-          reject(res);
+            icon: 'none',
+            title: (res.data as IData<T>).msg || '请求错误'
+          })
+          reject(res)
         }
       },
       //响应失败
       fail(err) {
-        reject(err);
+        reject(err)
         uni.showToast({
-          icon: "none",
-          title: "网络错误，换个网络试试~",
-        });
-      },
-    });
-  });
-};
+          icon: 'none',
+          title: '网络错误，换个网络试试~'
+        })
+      }
+    })
+  })
+}
